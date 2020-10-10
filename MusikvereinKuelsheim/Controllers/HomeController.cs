8d45b36f58f5e48d32f1e06147636e7194d6c1f3
Blackswater <Raphaellawo@gmx.de>
2020-10-10 17:33:45 +0200

    Datenschutz page, header und footer muss noch gemacht werden

--- MusikvereinKuelsheim/Controllers/HomeController.cs
+++ MusikvereinKuelsheim/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace MusikvereinKuelsheim.Controllers
             return View();
         }
 
+        public IActionResult Datenschutz()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
